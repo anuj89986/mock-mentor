1248cf215709a2bb61bc4d66d508370b0b72357f
anuj89986 <anuj89986@gmail.com>
2026-04-03 18:31:03 +0530

    add

---
+++ src/app/api/session/create-questions/route.ts
@@ -0,0 +1,36 @@
+export const runtime = "nodejs";
+
+import { generateQuestions,parseQuestions } from "@/lib/gemini";
+import { NextResponse } from "next/server";
+import { log } from "node:console";
+import pdf from "pdf-parse/lib/pdf-parse.js";
+
+export async function POST(req: Request) {
+  const formData = await req.formData();
+  const fileEntry = formData.get("resume") as File | null;
+
+  if (!fileEntry) {
+    return NextResponse.json({ error: "No file uploaded" }, { status: 400 });
+  }
+
+  if (fileEntry.type !== "application/pdf") {
+    return NextResponse.json(
+      { error: "Only PDF files are allowed" },
+      { status: 400 }
+    );
+  }
+
+    const buffer = Buffer.from(await fileEntry.arrayBuffer());
+    const result = await pdf(buffer);
+    if(!result.text || result.text.trim() === ""){
+        return NextResponse.json({ error: "Unable to extract text from PDF" }, { status: 400 });
+    }
+  try {
+    const questions = await generateQuestions(result.text);
+    const parsedQuestions = await parseQuestions(questions);
+    return NextResponse.json({ questions: parsedQuestions });
+  } catch (error) {
+    console.error("Error generating questions:", error);
+    return NextResponse.json({ error: "Error generating questions" }, { status: 500 });
+  }
+}
