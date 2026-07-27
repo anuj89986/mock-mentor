0a16d5f0a39474e4a82bb008eda54b9098f3ea8c
anuj89986 <anuj89986@gmail.com>
2026-07-27 22:19:27 +0530

    first push to deploy

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,15 @@
-FROM node:22
+FROM node:22-alpine
+
 WORKDIR /app
+
 COPY package*.json ./
-RUN npm install
+
+RUN npm ci
+
 COPY . .
+
 RUN npm run build
+
 EXPOSE 3000
-CMD ["npm","start"]
+
+CMD ["npm", "start"]
--- src/proxy.ts
+++ src/proxy.ts
@@ -3,26 +3,26 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export async function proxy(request: NextRequest) {
-  // const pathname = request.nextUrl.pathname;
-  // const publicRoutes = [
-  //   '/',
-  //   '/auth/signin',
-  //   '/auth/register'
-  // ];
-  // if(publicRoutes.includes(pathname)){
-  //   return NextResponse.next();
-  // }
-  // const token = await getToken({ req: request, secret: process.env.NEXTAUTH_SECRET });
-  // if (!token) {
-  //   console.log(`Unauthorized access attempt to: ${pathname}`);
-  //   const signInUrl = new URL('/auth/signin', request.url);
-  //   return NextResponse.redirect(signInUrl);
-  // }
+  const pathname = request.nextUrl.pathname;
+
+  const publicRoutes = [
+    "/",
+    "/auth/signin",
+    "/auth/register",
+  ];
+
+  if (publicRoutes.includes(pathname)) {
+    return NextResponse.next();
+  }
+
+  const token = await getToken({
+    req: request,
+    secret: process.env.NEXTAUTH_SECRET,
+  });
+
+  if (!token) {
+    return NextResponse.redirect(new URL("/auth/signin", request.url));
+  }
+
   return NextResponse.next();
 }
-
-export const config = {
-  matcher: ["/((?!api/auth/|_next/static|_next/image|favicon.ico).*)"],
-};
-
-
